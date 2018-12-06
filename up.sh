635ca270caa0f85b3299666a3896f9fbfec7e9d3
Tristone Chen <trist725@live.cn>
2018-12-06 19:49:47 +0800

    update up.sh

--- up.sh
+++ up.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-kill -9 $(ps -ef | grep mlgs.* | grep -v grep  | head -n 1 | awk '{print $2}')
+pid=`ps -ef | grep mlgs.* | grep -v grep  | head -n 1 | awk '{print $2}'`
 
+if [ -n "$pid" ]
+then
+
+    kill -9 ${pid}
+    echo "pid killed: " ${pid}
+
+fi
 
 cd bin
-(./mlgs* &)
+( ./mlgs* & )
